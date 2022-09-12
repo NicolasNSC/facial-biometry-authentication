52be3cc1da13e15a89690de299a34df6c09794fc
Nicolas Castanha <nicolascastanha2002@gmail.com>
2022-09-12 13:28:35 -0300

    create dataController

---
+++ dataController.py
@@ -0,0 +1,36 @@
+from multiprocessing import connection
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+def create_db_connection(host_name, user_name, user_password, db_name) :
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection succesful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created sucessfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successfull")
+    except Error as err:
+        print(f"Error: '{err}'")
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from dataController import create_database, create_db_connection
 
+
+pssword = "" 
+connection = create_db_connection("localhost", "host", pssword, "database")
+#create_database_query = "CREATE DATABASE school"
+#create_database(connection, create_database_query)
